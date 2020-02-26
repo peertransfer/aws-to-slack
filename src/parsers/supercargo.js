b4d8d5502edd65be22c2c17b59f0008bcdac9dd7
David Lluna <llunaplanet@gmail.com>
2020-02-26 16:24:46 +0100

    Adds support for a custom parser

---
+++ src/parsers/supercargo.js
@@ -0,0 +1,25 @@
+//
+// Supercargo event parser
+//
+exports.matches = event =>
+	_.get(event.message, 'slack_channel') === '#victoria-monitoring';
+
+exports.parse = event => {
+	
+	const payload = _.clone(event.message);
+  let title = event.getSubject();
+
+  slack_message_with_channel = event.attachmentWithDefaults({
+		fallback: payload.message,
+		color: payload.options.color,
+		author_name: "Victoria Supercargo",
+		title: title,
+		text: payload.message,
+	});
+  
+  slack_message_with_channel.channel = payload.slack_channel
+  slack_message_with_channel.hookUrl = payload.slack_hook_url
+  
+	return slack_message_with_channel
+
+};
